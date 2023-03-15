f7e079ad8487a76cb4a4347cf484f6ea8ef07cec
Enes Kaya <eneskaya5261@gmail.com>
2023-03-15 09:58:13 +0300

    feat(App.js): add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import ContactPage from "./pages/ContactPage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductsPage from "./pages/ProductsPage";
+import ProductPage from "./pages/ProductPage.js";
+import AboutProduct from "./pages/AboutProduct.js";
+import ProjectsPage from "./pages/ProjectsPage.js";
+import AboutProject from "./pages/AboutProject.js";
+import ContactPage from "./pages/ContactPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <ContactPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/urunler" element={<ProductsPage/>}/>
+        <Route path="/urun-sayfasi" element={<ProductPage/>}/>
+        <Route path="/urun-hakkinda" element={<AboutProduct/>}/>
+        <Route path="/projeler" element={<ProjectsPage/>}/>
+        <Route path="/proje-hakkinda" element={<AboutProject/>}/>
+        <Route path="/iletisim" element={<ContactPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
