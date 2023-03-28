61966d6acdc871970f204106dc91dab7222c6ec2
Enes Kaya <eneskaya5261@gmail.com>
2023-03-28 10:46:31 +0300

    style(header): update style

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
   let lng = localStorage.getItem("i18nextLng");
   return (
     <div className="h-screen  flex text-center">
-      <span className="absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-8xl text-6xl text-custom-white font-[Montserrat] selection:bg-custom-theme selection:text-custom-white md:border-b-4 border-b-2 border-custom-theme pb-5">
+      <span className="w-72 md:w-auto absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-8xl text-6xl text-custom-white font-[Montserrat] selection:bg-custom-theme selection:text-custom-white md:border-b-4 border-b-2 border-custom-theme pb-5">
         ARC Yapı
       </span>
       <span className="w-full absolute md:top-[60%] top-[55%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-5xl text-3xl text-custom-white italic font-[Bitter] selection:bg-custom-theme selection:text-custom-white">
