19d217150bec842da32cc9bbc0b4b86be0cb052a
Enes Kaya <eneskaya5261@gmail.com>
2023-03-28 17:40:12 +0300

    feat(Header.js): update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ function Header() {
   let lng = localStorage.getItem("i18nextLng");
   return (
     <div className="h-screen  flex text-center">
-      <span className="w-72 md:w-auto absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-8xl text-6xl text-custom-white font-[Montserrat] selection:bg-custom-theme selection:text-custom-white md:border-b-4 border-b-2 border-custom-theme pb-5">
+      <span className="w-72 md:w-auto absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-8xl text-6xl text-custom-theme font-medium font-[Montserrat] selection:bg-custom-theme selection:text-custom-white md:border-b-4 border-custom-white pb-5">
         ARC Yapı
       </span>
-      <span className="w-full absolute md:top-[60%] top-[55%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-5xl text-3xl text-custom-white italic font-[Bitter] selection:bg-custom-theme selection:text-custom-white">
+      <span className="bg-custom-section1 bg-opacity-90 py-3 w-full absolute md:top-[60%] top-[55%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-5xl text-3xl text-custom-theme italic font-medium font-[Bitter] selection:bg-custom-theme selection:text-custom-white">
         {common[0].header[`${lng}`]}
       </span>
-      <img className="w-full h-full object-cover" src="/assets/bg.jpg" alt="" />
+      <img className="w-full h-full object-cover" src="/assets/house.jpg" alt="" />
     </div>
   );
 }
